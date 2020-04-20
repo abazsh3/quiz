58d5cc6ed4f1cdceeb3294e91640107620409356
abazsh3 <abazsh3@gmail.com>
2020-04-20 18:32:34 +0430

    ng

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const app = express();
 app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.use(cors());
 //app.use("/src", express.static('src'));
-
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use("/user",usersRoute);
 
 
